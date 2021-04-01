6f87d8fb6f0e4dfb3ad3ad8d9033a3b1020aa541
hazicatalin <61392637+hazicatalin@users.noreply.github.com>
2021-04-02 02:53:37 +0300

    read from db

--- src/news_routes.py
+++ src/news_routes.py
@@ -1,5 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from mysql import connector
+import json
+
+
 simple_page = Blueprint('simple_page', __name__, template_folder='templates')
+
+
 @simple_page.route('/news')
 def get_news():
-    return "stiri"
+    connection=connector.connect(
+        host = "localhost",
+        user = "covid-19-user",
+        password = "covid-19-pass",
+        db = "CC_T3"
+    )
+    cursor = connection.cursor()
+    cursor.execute("select * from Vaccine_Type")
+    #return json.loads(str(cursor.fetchall()))
+    response = jsonify(cursor.fetchall())
+    cursor.close()
+    connection.close()
+    return response
