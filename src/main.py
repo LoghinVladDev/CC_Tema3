ad01a26f8b526e4ca4f8f24409ffb95b8d1b61f2
LoghinVladDev <vlad.loghin00@gmail.com>
2021-04-02 17:36:09 +0300

    Missing Page in main

--- src/main.py
+++ src/main.py
@@ -4,11 +4,13 @@ from src.vaccination_appointment_routes import simple_page_4
 from src.hospital_admission_routes import simple_page_5
 from src.hospital_opinion_routes import simple_page_6
 from src.vaccine_type_routes import simple_page_7
+from src.vaccination_centre_routes import simple_page_3
 from flask import Flask
 
 app = Flask(__name__)
 app.register_blueprint(simple_page)
 app.register_blueprint(simple_page_2)
+app.register_blueprint(simple_page_3)
 app.register_blueprint(simple_page_4)
 app.register_blueprint(simple_page_5)
 app.register_blueprint(simple_page_6)
