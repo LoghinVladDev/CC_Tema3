004e227f81a9106a8aa532ef02fa01c9a41f924f
hazicatalin <61392637+hazicatalin@users.noreply.github.com>
2021-04-02 13:02:21 +0300

    hospital routes

--- src/vaccination_centre_routes.py
+++ src/vaccination_centre_routes.py
@@ -2,10 +2,9 @@ from flask import Blueprint, jsonify, request
 from mysql import connector
 
 
-simple_page_2 = Blueprint('simple_page_2', __name__, template_folder='templates')
+simple_page_3 = Blueprint('simple_page_2', __name__, template_folder='templates')
 
-
-@simple_page_2.route('/hospital', methods = ["GET", "POST"])
+@simple_page_2.route('/vaccination_centre', methods = ["GET", "POST"])
 def hospital():
     if request.method == "GET":
         connection=connector.connect(
@@ -16,7 +15,7 @@ def hospital():
         )
         cursor = connection.cursor()
         try:
-            cursor.execute("select * from Hospital")
+            cursor.execute("select * from Vaccination_Centre")
             response = jsonify(cursor.fetchall())
         except Exception as e:
             response = jsonify({"status": "failure"})
@@ -32,11 +31,12 @@ def hospital():
             password = "covid-19-pass",
             db = "CC_T3"
         )
-        name = request.form['name']
         address = request.form['address']
+        dose_count = request.form['dose_count']
+        dose_type_ID = request.form['name']
         cursor = connection.cursor()
         try:
-            cursor.execute("insert into Hospital (name, address) values (%s, %s)",(name, address))
+            cursor.execute("insert into Vaccination_Centre (address, dose_count, dose_type_ID) values (%s, %s)",(name, address))
             connection.commit()
             response = jsonify({"status": "success"})
         except Exception as e:
@@ -45,19 +45,3 @@ def hospital():
         cursor.close()
         connection.close()
         return response
-
-
-@simple_page_2.route('/hospital/<string:name>')
-def get_hospital_name(name):
-    connection = connector.connect(
-        host="localhost",
-        user="covid-19-user",
-        password="covid-19-pass",
-        db="CC_T3"
-    )
-    cursor = connection.cursor()
-    cursor.execute("select * from Hospital where name = %s", (name,))
-    response = jsonify(cursor.fetchall())
-    cursor.close()
-    connection.close()
-    return response
